82554991a231044642db6cfe60de0c0f442b8275
Satyal Yadav <satyalyadav10@gmail.com>
2024-09-30 14:47:50 -0700

    Commit for movinf grom Windows to Linux

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
   
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -102,7 +103,7 @@ const AuthForm = ({ type }: { type: string }) => {
         </header>
         {user ? (
             <div className='flex flex-col gap-4'>
-                {/* PlaidLink */}
+                <PlaidLink user={user} variant="primary" />
             </div>
         ): (
             <>
