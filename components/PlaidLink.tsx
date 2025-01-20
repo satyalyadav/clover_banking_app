daeff81a19f6fcea2e6ed5e4a6c4f54b3fe552f0
Satyal Yadav <satyalyadav10@gmail.com>
2025-01-19 17:47:42 -0700

    PlaindLink and Dwolla

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
+import { useRouter } from 'next/navigation'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
 
-const PlaidLink = ({ user, variant }: PlaidLinkProps) => 
-{
-  return (
-    <>
-        {variant === 'primary' ? (
-            <Button>
-                Connect bank
-            </Button>
-        ): variant === 'ghost' ? (
-            <Button>
-                Connect bank
-            </Button>
-        ): (
-            <Button>
-                Connect bank
-            </Button>
-        )}
-    </>
-  )
-}
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+    const router = useRouter();
+    
+    const [token, settoken] = useState('');
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+            const data = await createLinkToken(user);
+
+            settoken(data?.linkToken);
+        }
+
+        getLinkToken();
+    }, [])
+
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
+        await exchangePublicToken({
+            publicToken: public_token,
+            user,
+        })
+
+        router.push('/');
+    }, [user])
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess
+    }
+
+    const { open, ready } = usePlaidLink(config);
+
+    return (
+        <>
+            {variant === 'primary' ? (
+                <Button onClick={() => open()} disabled={!ready} className='plaidlink-primary'>
+                    Connect bank
+                </Button>
+            ): variant === 'ghost' ? (
+                <Button>
+                    Connect bank
+                </Button>
+            ): (
+                <Button>
+                    Connect bank
+                </Button>
+            )}
+        </>
+      )
+    }
 
 export default PlaidLink
